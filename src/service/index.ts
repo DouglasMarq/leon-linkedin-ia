50a5f1eab260b271f8c4c17a76112d409d84a13f
DouglasMarq <douglas.marq.alves@outlook.com>
2022-02-21 20:10:37 -0300

    queue: lint

--- src/service/index.ts
+++ src/service/index.ts
@@ -7,32 +7,32 @@ import Log from '../utils/log';
 
 @injectable()
 export default class Core {
-    private readonly _cron;
+  private readonly _cron;
 
-    constructor(@inject(Invites) private readonly invite: Invites,
+  constructor(@inject(Invites) private readonly invite: Invites,
     @inject(Log) private readonly console: Log) {
-        this._cron = Cron;
-    }
+    this._cron = Cron;
+  }
 
-    async setup() {
-        this._cron.schedule(`* * * * *`, async () => {
-            this.console.log(`Starting Invite verification`);
+  async setup() {
+    this._cron.schedule(`* * * * *`, async () => {
+      this.console.log(`Starting Invite verification`);
 
-            let user: Invite[] = (await this.invite.getInvites).map((item: any) => {
-                return {
-                    id: item.profile.profileId,
-                    name: `${item.profile.firstName} ${item.profile.lastName}`,
-                    sentDate: format(new Date(item.sentTime), "dd/MM/yyyy"),
-                    message: item.message || null,
-                    invitationId: item.mailboxItemId.split(":")[item.mailboxItemId.split(":").length-1],
-                    invitationSharedSecret: item.sharedSecret,
-                }
-            });
+      let user: Invite[] = (await this.invite.getInvites).map((item: any) => {
+        return {
+          id: item.profile.profileId,
+          name: `${item.profile.firstName} ${item.profile.lastName}`,
+          sentDate: format(new Date(item.sentTime), "dd/MM/yyyy"),
+          message: item.message || null,
+          invitationId: item.mailboxItemId.split(":")[item.mailboxItemId.split(":").length - 1],
+          invitationSharedSecret: item.sharedSecret,
+        }
+      });
 
-            if(user && user.message) this.invite.acceptInviteAndSendMessage(user.invitationId, user.invitationSharedSecret, user.id, user.name, user.message);
-            else if (user && !user.message) this.invite.acceptInvite(user.invitationId, user.invitationSharedSecret);
+      if (user && user.message) this.invite.acceptInviteAndSendMessage(user.invitationId, user.invitationSharedSecret, user.id, user.name, user.message);
+      else if (user && !user.message) this.invite.acceptInvite(user.invitationId, user.invitationSharedSecret);
 
-            this.console.log(`Finished Invite verification`);
-        });
-    }
+      this.console.log(`Finished Invite verification`);
+    });
+  }
 }
--- src/service/invites.ts
+++ src/service/invites.ts
@@ -5,50 +5,50 @@ import WordBlacklist from "../utils/blacklist";
 
 @injectable()
 export default class Invites {
-    private words: string[];
+  private words: string[];
 
-    constructor(@inject(Client) private readonly client: Client,
+  constructor(@inject(Client) private readonly client: Client,
     @inject(Messages) private readonly messages: Messages,
     @inject(WordBlacklist) private readonly wordBL: WordBlacklist) {
-        this.words = wordBL.wordBlacklist;
-       }
+    this.words = wordBL.wordBlacklist;
+  }
 
-    async acceptInvite(invitationId: string, invitationSharedSecret: string) {
-        return this.client.request.post(`relationships/invitations/${invitationId}?action=accept`, {
-            invitationId: invitationId,
-            invitationSharedSecret: invitationSharedSecret,
-            isGenericInvitation: false
-        });
+  async acceptInvite(invitationId: string, invitationSharedSecret: string) {
+    return this.client.request.post(`relationships/invitations/${invitationId}?action=accept`, {
+      invitationId: invitationId,
+      invitationSharedSecret: invitationSharedSecret,
+      isGenericInvitation: false
+    });
+  }
+
+  async acceptInviteAndSendMessage(invitationId: string,
+    invitationSharedSecret: string,
+    profileId: string,
+    profileName: string,
+    message: string | null = null) {
+    let passedWords: string = "";
+    if (message) message = message.toUpperCase();
+
+    await this.acceptInvite(invitationId, invitationSharedSecret);
+
+    if (!message) return;
+
+    for (let i in this.words) {
+      if (message.includes(this.words[i])) passedWords += `${this.words[i]} `;
     }
 
-    async acceptInviteAndSendMessage(invitationId: string,
-        invitationSharedSecret: string,
-        profileId: string,
-        profileName: string,
-        message: string | null = null) {
-        let passedWords: string = "";
-        if (message) message = message.toUpperCase();
+    const text = this.messages.greetings(profileName, passedWords);
 
-        await this.acceptInvite(invitationId, invitationSharedSecret);
+    this.sendMessage(profileId, text);
+  }
 
-        if (!message) return;
+  private sendMessage(profileId: string, text: string) {
+    setTimeout(async () => {
+      return this.client.message.sendMessage({ profileId: profileId, text: text });
+    }, 4000);
+  }
 
-        for(let i in this.words) {
-            if(message.includes(this.words[i])) passedWords += `${this.words[i]} `;
-        }
-
-        const text = this.messages.greetings(profileName, passedWords);
-
-        this.sendMessage(profileId, text);
-    }
-
-    private sendMessage(profileId: string, text: string) {
-        setTimeout(async () => {
-            return this.client.message.sendMessage({profileId: profileId, text: text});
-        }, 4000);
-    }
-
-    get getInvites() {
-        return this.client.invitation.getReceivedInvitations().scrollNext();
-    }
+  get getInvites() {
+    return this.client.invitation.getReceivedInvitations().scrollNext();
+  }
 }
